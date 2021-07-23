e6c33721e5869c3cf6f5616521458e15b9e5b451
WilliamZupper <william.assis@zup.com.br>
2021-07-23 17:49:31 -0300

    projeto proposta configurado

---
+++ proposta/src/test/java/br/com/william/ot6/proposta/PropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.william.ot6.proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
